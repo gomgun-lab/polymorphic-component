053760906012f3fd8ac285d71df393050f3c2e17
Choi Hong Gyu <zxolqs@gmail.com>
2023-05-27 09:23:33 +0900

    add test code for as

--- src/Polymorphic/index.test.tsx
+++ src/Polymorphic/index.test.tsx
@@ -17,6 +17,12 @@ describe("Button component", () => {
     expect(buttonElement).toHaveStyle("color: red");
   });
 
+  test("render button as div", () => {
+    render(<Button as="div">Click me</Button>);
+    const buttonElement = screen.getByText(/Click me/i);
+    expect(buttonElement.tagName).toBe("DIV");
+  });
+
   test("forwards ref correctly", () => {
     const ref = React.createRef<HTMLButtonElement>();
     render(<Button ref={ref}>Click me</Button>);
